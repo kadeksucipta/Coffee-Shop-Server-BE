78c58542c5359f2975706600b738f62deea6dd3d
kadek12345 <kadek.sucipta005@gmail.com>
2023-05-13 13:56:03 +0800

    delete cors options

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
 app.use(cors())
 app.use(logger('dev'));
